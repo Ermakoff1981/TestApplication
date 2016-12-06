49f6f130428e916893ddf9f20c69a14938f3119c
Maxim Ermakov <ermakov@steelkiwi.com>
2016-12-07 00:37:47 +0200

    template

--- app/src/main/java/com/ermakoff1981/user/testapplication/MainActivity.java
+++ app/src/main/java/com/ermakoff1981/user/testapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.ermakoff1981.user.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText input;
+    private TextView output;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText)findViewById(R.id.input);
+        output = (TextView)findViewById(R.id.output);
+        okButton = (Button)findViewById(R.id.okbutton);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processText(input.getText().toString().trim());
+            }
+        });
+    }
+
+    private void processText(String input) {
+        //TODO some processing here
+
+
+        output.setText(input);
     }
 }
