90be71bb5e72273d4090309da2d2f4f3f67177d3
Ermakoff1981 <ermakoff1981@gmail.com>
2016-12-07 00:14:39 +0200

    initial

---
+++ app/src/main/java/com/ermakoff1981/user/testapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ermakoff1981.user.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
